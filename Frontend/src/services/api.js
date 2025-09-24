0ab4661fd998eb2b68f39a909ff9e1a3bfaa834b
Aditya <ajha48450@gmail.com>
2025-09-24 14:48:47 +0530

    some

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bookswap-backend-tiff.onrender.com"
+  baseURL: "https://bookswap-backend-tiff.onrender.com/api"
 });
 
 api.interceptors.request.use(config => {
